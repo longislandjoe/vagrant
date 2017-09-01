da377a34152d5f8f65883a10fe0ef410c3088f50
Joe R <longislandjoe@gmail.com>
2017-09-01 11:32:14 -0400

    Adding web content

---
+++ scripts/centos-lamp.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yum update -y --exclude=kernel
+
+yum install -y nano git unzip screen
+
+yum install -y httpd httpd-devel httpd-tools
+chkconfig --add httpd
+chkconfig httpd on
+service httpd stop
+
+rm -rf /var/www/html
+ln -s /vagrant /var/www/html
+
+service httpd start
+
+yum install -y php php-cli php-common php-devel php-mysql
+
+yum install -y mysql mysql-server mysql-devel
+chkconfig --add mysqld
+chkconfig mysqld on
+
+service mysqld start
+
+mysql -u root -e "SHOW DATABASES";
+
+service httpd restart
+
